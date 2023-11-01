a8e6fd8b713d7fe12a61109bfa82cebc064f79b3
VotinovVladimir <CodingGeek@yandex.ru>
2023-11-01 09:21:57 +0300

    Выпуск 11. Что такое сеттеры и геттеры.

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -6,10 +6,10 @@ public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so;
         so = new StudentOrder();
-        so.hFirstName = "Алексей";
-        so.hLastName = "Петров";
-        so.wFirstName = "Галина";
-        so.wLastName = "Петрова";
+        so.sethFirstName("Алексей");
+        so.sethLastName("Петров");
+        so.setwFirstName("Галина");
+        so.setwLastName("Петрова");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -18,7 +18,7 @@ public class SaveStudentOrder {
 
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 199;
-        System.out.println("saveStudentOrder");
+        System.out.println("saveStudentOrder:" + studentOrder.gethLastName());
         return answer;
     }
 }
